49526d1d438b3337cb0f179c59a7aebc1700c5b9
SofiiaHyriak <Sofiia.Hyriak@dataart.com>
2026-01-07 20:19:49 +0200

    Added calculator app functionality

---
+++ DivideByZeroEx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    public class DivideByZeroEx: Exception
+    {
+     public DivideByZeroEx(string message) : base(message)
+        {
+         
+        }
+     
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CalculatorApp
+{
+
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            Calculator calculator = new Calculator();
+            double sum = calculator.Add(5, 3);
+            Console.WriteLine("Add result {0} + {1} = {2}", 5, 3, sum);
+        }
+    }
+}
+
